da0e6d4dc29037ed5a95a0a9c7586c8d87f215f2
TeaSpillStudios <teaspillstudios@proton.me>
2022-11-03 19:59:49 +0000

    Add cleanup and copy functions

--- main.sh
+++ main.sh
@@ -1,1 +1,24 @@
 #!/usr/bin/env bash
+
+cleanup() {
+  mv ~/.config/alacritty ~/.config/alacritty.bak
+  mv ~/.config/dunst ~/.config/dunst.bak
+  mv ~/.config/i3 ~/.config/i3.bak
+  mv ~/.config/nvim ~/.config/nvim.bak
+  mv ~/.config/picom ~/.config/picom.bak
+  mv ~/.config/polybar ~/.config/polybar.bak
+  mv ~/.config/rofi ~/.config/rofi.bak
+}
+
+copy() {
+  mv ./alacritty ~/.config/alacritty.bak
+  mv ./dunst ~/.config/dunst.bak
+  mv ./i3 ~/.config/i3.bak
+  mv ./nvim ~/.config/nvim.bak
+  mv ./picom ~/.config/picom.bak
+  mv ./polybar ~/.config/polybar.bak
+  mv ./rofi ~/.config/rofi.bak
+}
+
+cleanup
+copy
