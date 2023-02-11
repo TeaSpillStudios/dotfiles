559c99288a7df3c1a27de9b2cabf77f7793ba0eb
TeaSpillStudios <teaspillstudios@proton.me>
2023-02-11 22:21:38 +0000

    Add lightdm theme

--- setup-dependencies.sh
+++ setup-dependencies.sh
@@ -19,13 +19,25 @@ xautolock() {
 }
 
 paru() {
-	cd /opt
+	cd /opt/
 	sudo git clone https://aur.archlinux.org/paru-git.git
 	sudo chown $(whoami):$(whoami) -R ./paru-git/
 	cd ./paru-git/
 	makepkg -si
 }
 
+lightdm-theme() {
+	cd /opt/
+	sudo git clone https://aur.archlinux.org/lightdm-webkit2-theme-glorious.git
+	sudo chown $(whoami):$(whoami) -R ./lightdm-webkit2-theme-glorious
+	cd lightdm-webkit2-theme-glorious
+	makepkg -sri --noconfirm
+	sudo sed -i 's/^\(#?greeter\)-session\s*=\s*\(.*\)/greeter-session = lightdm-webkit2-greeter #\1/ #\2g' /etc/lightdm/lightdm.conf
+	sudo sed -i 's/^webkit_theme\s*=\s*\(.*\)/webkit_theme = glorious #\1/g' /etc/lightdm/lightdm-webkit2-greeter.conf
+	sudo sed -i 's/^debug_mode\s*=\s*\(.*\)/debug_mode = true #\1/g' /etc/lightdm/lightdm-webkit2-greeter.conf
+}
+
 i3lock-color
 xautolock
 paru
+lightdm-theme
